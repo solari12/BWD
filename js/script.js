d97bf763ac6fe07fc9b5906110968f9eb06d5808
solari12 <vantuan11102004@gmail.com>
2023-05-17 21:05:30 +0700

    them button darklight

--- js/script.js
+++ js/script.js
@@ -14,3 +14,23 @@ document.querySelectorAll('.about .video-container .controls .control-btn').forE
         document.querySelector('.about .video-container .video').src = src;
     }
 })
+const blogs = document.getElementById('blogs');
+const gallery = document.getElementById('gallery');
+const services = document.getElementById('services');
+const destination = document.getElementById('destination');
+const header = document.querySelector('header');
+const book = document.getElementById('book-form');
+const body = document.querySelector('body');
+const toggle = document.getElementById('toggle');
+const about = document.getElementById('about');
+toggle.onclick = function(){
+    toggle.classList.toggle('active');
+    header.classList.toggle('active');
+    body.classList.toggle('active');
+    book.classList.toggle('active');
+    about.classList.toggle('active');
+    destination.classList.toggle('active');
+    services.classList.toggle('active');
+    gallery.classList.toggle('active');
+    blogs.classList.toggle('active');
+}
